206f6ed7a8c389fe68a74fde519822cb98b6740f
ranselorm <ranselorm@outlook.com>
2026-01-01 20:55:00 +0000

    Add set example and update comments for tuple immutability

--- setup.py
+++ setup.py
@@ -30,8 +30,15 @@ print("List example:", list_example)
 print("Length of tuple:", len(tuple_example))
 print("Length of list:", len(list_example))
 
-tuple_example.append(20)  # This will raise an AttributeError since tuples are immutable
+# tuple_example.append(20)  # This will raise an AttributeError since tuples are immutable
 list_example.append(60)   # This will work since lists are mutable
+
+# Set example
+set_example = {1, 2, 3, 4, 5}
+set_example.add(6)  # Adding an element to the set
+print("Set example after adding an element:", set_example)
+set_example.remove(3)  # Removing an element from the set
+print("Set example after removing an element:", set_example)
 
 
 
