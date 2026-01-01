08341d01143277c0a650e41746261b45c74ac057
ranselorm <ranselorm@outlook.com>
2026-01-01 05:41:04 +0000

    Initilaize repository

---
+++ setup.py
@@ -0,0 +1,25 @@
+import math
+
+# def greeting(name):
+#     print(f"Hello, {name}, have a nice day!")
+    
+# user_name = str(input("Enter your name: "))
+
+# if user_name:
+#     greeting(user_name)
+# else:
+#     print("No name provided.")  
+
+
+
+
+# def two_numbers(height, width):
+#     return (height ** 2 + width ** 2) ** 0.5
+  
+
+# numbers = int(input("Enter first number: ")), int(input("Enter second number: "))
+
+# result = int(two_numbers(*numbers))
+# print(f"The hypotenuse of the right triangle with sides {numbers} is: {result}")
+
+
